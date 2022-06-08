c582085f58653273b7c18cffa1b0055d78db3c64
Daniel Sanches <danielsanches6301@gmail.com>
2022-06-08 20:27:28 -0300

    Ajustanto controller e adicionando entity framework

---
+++ Back/src/ProEventos.Api/Controllers/EventoController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using ProEventos.Api.Models;
+
+namespace ProEventos.Api.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class EventoController : ControllerBase
+    {
+        public EventoController()
+        {
+        }
+
+        [HttpGet]
+        public IEnumerable<Evento> Get()
+        {
+            return new Evento[]
+            {
+                new Evento
+                {
+                    Tema = "Angular 11",
+                    DataEvento = DateTime.Now.AddDays(7).ToString(),
+                    EventoId = 1,
+                    ImagemUrl = "",
+                    Local = "Aqui",
+                    Lote = "",
+                    QtdPessoas = 250
+                }
+            };
+        }
+    }
+}
